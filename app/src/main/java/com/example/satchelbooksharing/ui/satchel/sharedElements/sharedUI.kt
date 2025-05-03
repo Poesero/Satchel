94525139717846c850a2f3d3c81e66422f43066a
Poesero <martinferreira09@hotmail.com>
2025-05-03 14:41:48 -0300

    sharesUI.kt added

---
+++ app/src/main/java/com/example/satchelbooksharing/ui/satchel/sharedElements/sharedUI.kt
@@ -0,0 +1,13 @@
+package com.example.satchelbooksharing.ui.satchel.sharedElements
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Header (){
+
+}
+
+@Composable
+fun Footer (){
+
+}
