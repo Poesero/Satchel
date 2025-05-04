03704bd8589bebfa2ef48ecefefa8c11dbcc666a
Poesero <martinferreira09@hotmail.com>
2025-05-04 14:42:28 -0300

    Header and Footer, seeing how it works by putting it to test in the home screen.

--- app/src/main/java/com/example/satchelbooksharing/ui/satchel/screens/HomeScreen.kt
+++ app/src/main/java/com/example/satchelbooksharing/ui/satchel/screens/HomeScreen.kt
@@ -5,13 +5,27 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.satchelbooksharing.ui.satchel.sharedElements.Footer
+import com.example.satchelbooksharing.ui.satchel.sharedElements.Header
 
 @Composable
 fun HomeScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ){
-        Text(text = "Bienvenido a Satchel HomeScreen")
+    Column (
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Header(title = "the Home screen this is")
+
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(24.dp)
+        ) {
+            //Contenido principal en algun momento
+            Text("Contenido principal en algun momento")
+        }
+        
+        Footer()
     }
 }
--- app/src/main/java/com/example/satchelbooksharing/ui/satchel/sharedElements/sharedUI.kt
+++ app/src/main/java/com/example/satchelbooksharing/ui/satchel/sharedElements/sharedUI.kt
@@ -1,13 +1,61 @@
 package com.example.satchelbooksharing.ui.satchel.sharedElements
 
+import android.inputmethodservice.Keyboard.Row
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun Header (){
-
+fun Header (title: String) {
+    Surface(
+        tonalElevation = 4.dp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp)
+    ) {
+        Box(
+            contentAlignment = Alignment.CenterStart,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp)
+        ) {
+            Text(
+                text = title,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
 }
 
 @Composable
 fun Footer (){
-
+    Surface(
+        tonalElevation = 4.dp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment  = Alignment.CenterVertically
+        ) {
+            Text("© Satchel 2025", fontSize = 14.sp)
+            Text("Hecho con 💙", fontSize = 14.sp)
+        }
+    }
 }
