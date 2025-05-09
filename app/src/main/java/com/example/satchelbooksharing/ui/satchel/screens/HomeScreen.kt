98f385774fdd9b9c19ea4a624016b55086874bb5
Poesero <martinferreira09@hotmail.com>
2025-05-09 13:00:25 -0300

    home screen preview added

--- app/src/main/java/com/example/satchelbooksharing/ui/satchel/screens/HomeScreen.kt
+++ app/src/main/java/com/example/satchelbooksharing/ui/satchel/screens/HomeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.satchelbooksharing.ui.satchel.sharedElements.Footer
 import com.example.satchelbooksharing.ui.satchel.sharedElements.Header
@@ -28,4 +29,10 @@ fun HomeScreen() {
         
         Footer()
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
 }
