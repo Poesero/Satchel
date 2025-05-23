cf207af3ef0ff68eb7d29b92469ad86a920ede74
Poesero <martinferreira09@hotmail.com>
2025-05-23 01:04:26 -0300

    Cards and containers

--- app/src/main/java/com/example/satchelbooksharing/ui/satchel/screens/LibraryScreen.kt
+++ app/src/main/java/com/example/satchelbooksharing/ui/satchel/screens/LibraryScreen.kt
@@ -1,4 +1,59 @@
 package com.example.satchelbooksharing.ui.satchel.screens
 
-class LibraryScreen {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.example.satchelbooksharing.ui.satchel.sharedElements.Footer
+import com.example.satchelbooksharing.ui.satchel.sharedElements.Header
+import com.example.satchelbooksharing.ui.satchel.sharedElements.SatchelBodyContainer
+
+@Composable
+fun LibraryScreen(navController: NavHostController) {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Header(title = "Library")
+
+        Column (
+            modifier = Modifier
+                .weight(1f)
+                .padding(24.dp, vertical = 12.dp)
+        ) {
+            SatchelBodyContainer {
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(3),
+                    modifier = Modifier.fillMaxSize(),
+                    contentPadding = PaddingValues(8.dp),
+                    verticalArrangement = Arrangement.spacedBy(3.dp),
+                    horizontalArrangement = Arrangement.spacedBy(3.dp)
+                ) {
+                    item{
+                        AddBookButton()
+                    }
+
+                    items(books.size) { index ->
+                    BookCard(
+                        title = books[index],
+                        author = "Autor ${index + 1}",
+                        imageProvided = false
+                    )
+                    }
+                }
+            }
+        }
+
+
+
+    }
 }
--- app/src/main/java/com/example/satchelbooksharing/ui/satchel/sharedElements/sharedUI.kt
+++ app/src/main/java/com/example/satchelbooksharing/ui/satchel/sharedElements/sharedUI.kt
@@ -1,13 +1,22 @@
 package com.example.satchelbooksharing.ui.satchel.sharedElements
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.Home
@@ -20,9 +29,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import com.example.satchelbooksharing.R
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -145,6 +159,67 @@ fun SearchPreview(){
     SearchField()
 }
 
+@Composable
+fun SatchelBodyContainer(
+    modifier: Modifier = Modifier,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Box (
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .clip(RoundedCornerShape(24.dp))
+            .background(MaterialTheme.colorScheme.surface)
+            .padding(16.dp)
+    ) {
+        Column (
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .fillMaxSize(),
+            content = content
+        )
+    }
+}
+
+@Composable
+fun BookCard(){
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Card(modifier = Modifier
+            .width(50.dp)
+            .height(80.dp),
+            elevation = CardDefaults.cardElevation(7.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )
+        ) {
+            Column (modifier = Modifier.fillMaxSize()){
+                Image(painter = painterResource(id = R.drawable.empty_image), contentDescription = "null")
+                Text(text = "<Title...>",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    modifier = Modifier.padding(6.dp),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+
+                Text(text = "Author...",
+                    fontSize = 13.sp,
+                    color = Color.LightGray,
+                    modifier = Modifier.padding(2.dp)
+                )
+            }
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewBookCard(){
+    BookCard()
+}
 
 
 /*
