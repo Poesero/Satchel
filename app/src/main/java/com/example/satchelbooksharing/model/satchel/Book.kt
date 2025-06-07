c07205ee962b5e1b15b852008a1d9978ee333628
Poesero <martinferreira09@hotmail.com>
2025-06-07 14:09:11 -0300

    Book model description added and methods changed in LibraryViewModel

--- app/src/main/java/com/example/satchelbooksharing/model/satchel/Book.kt
+++ app/src/main/java/com/example/satchelbooksharing/model/satchel/Book.kt
@@ -3,8 +3,9 @@ package com.example.satchelbooksharing.model.satchel
 data class Book (
     var title: String,
     var author: String,
+    var description: String,
     var genre: Genre = Genre.OTHER,
     val imageUri: String?
     )
 
-{ constructor() :this("","", Genre.OTHER,"")}
+{ constructor() :this("","","",Genre.OTHER,null)}
--- app/src/main/java/com/example/satchelbooksharing/viewModel/satchel/LibraryViewModel.kt
+++ app/src/main/java/com/example/satchelbooksharing/viewModel/satchel/LibraryViewModel.kt
@@ -5,21 +5,65 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.satchelbooksharing.data.LibraryRepository
+import com.example.satchelbooksharing.data.LocalLibraryRepository
 import com.example.satchelbooksharing.model.satchel.Book
+import com.example.satchelbooksharing.model.satchel.Genre
 import com.example.satchelbooksharing.model.satchel.UserLibrary
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
-class LibraryViewModel : ViewModel() {
+class LibraryViewModel(
+    private val repository: LibraryRepository = LocalLibraryRepository()
+) : ViewModel() {
+
+    val books: StateFlow<List<Book>> = repository.getBooks().stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = emptyList()
+    )
+
+    var title by mutableStateOf("")
+        private set
+
+    var author by mutableStateOf("")
+        private set
+
+    var description by mutableStateOf("")
+        private set
+
+    var genre by mutableStateOf("")
+        private set
 
     var currentLibrary by mutableStateOf(UserLibrary())
         private set
 
+    /*
     fun loadLibrary(userId: String) {
         viewModelScope.launch {
             currentLibrary = repository.getLibrary(userId)
         }
     }
+     */
 
+    fun onTitleChanged(newTitle : String) { title = newTitle}
+    fun onAuthorChanged(newAuthor : String) { author = newAuthor}
+    fun onDescriptionChanged(newDesc : String) { description = newDesc}
+    fun onGenreChanged(newGenre : Genre) { genre = newGenre.name}
+
+    fun saveBook(){
+        val book = Book(title, author, description, genre, null)
+    }
+
+    private fun resetFields() {
+        title = ""
+        author = ""
+        description = ""
+        genre = Genre.OTHER
+    }
+    /*
     fun addBook(book: Book){
         viewModelScope.launch {
             val userId = currentLibrary.user
@@ -27,6 +71,7 @@ class LibraryViewModel : ViewModel() {
             currentLibrary.collection.add(book)
         }
     }
+     */
 
     fun deleteBook(book: Book) {
         viewModelScope.launch {
